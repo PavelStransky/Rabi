ef9cb7d1e6129df7a1a711eb599cdc31fbcb86ce
Pavel Stránský <github@pavelstransky.cz>
2023-08-08 16:17:42 +0200

    Different parameters

--- tasks/LipkinWigner.jl
+++ tasks/LipkinWigner.jl
@@ -1,9 +1,10 @@
+const PATH = "d:\\results\\Lipkin\\"
+
 include("../Lipkin.jl")
 
-print(dense(Jz(Lipkin(j=10))))
-print(eigenstates(Lipkin(j=10, λ=0))[1])
-print(Size(Lipkin(j=10)))
+pyplot(size=(1200, 1000))
+
 
 lipkin = Lipkin(j=50, λ=1)
 LevelDynamics(lipkin; ps=LinRange(0, 3, 100), ylims=(-1.7, -0.6))
-LevelDynamics(lipkin; ps=LinRange(0, 1, 100), type = :ω)
+LevelDynamics(lipkin; ps=LinRange(0, 1, 100), type = :ω, ylims=(-1, 0.5), saveGraph=true)
