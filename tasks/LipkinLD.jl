98cec416f2a45984d2c957a005cb6e6c92e42382
Pavel Stránský <github@pavelstransky.cz>
2023-08-11 11:58:05 +0200

    Change dimension

--- tasks/LipkinLD.jl
+++ tasks/LipkinLD.jl
@@ -5,12 +5,17 @@ include("../Lipkin.jl")
 pyplot(size=(1200, 1000))
 
 # Habilitace
-lipkin = Lipkin(j=10)
+lipkin = Lipkin(j=8)
 x = LinRange(0, 1, 1000)
 e, p = LevelDynamics(lipkin; ps=x, type = :ξ, saveGraph=true)
 Export("d:\\qpt2", x, e)
 
-lipkin = Lipkin(j=10, ξ=0.6)
+lipkin = Lipkin(j=40)
+x = LinRange(0, 1, 1000)
+e, p = LevelDynamics(lipkin; ps=x, type = :ξ, saveGraph=true)
+Export("d:\\qpt2d", x, e)
+
+lipkin = Lipkin(j=8, ξ=0.6)
 x = LinRange(-5, 5, 2000)
 e, p = LevelDynamics(lipkin; ps=x, type = :ζ, saveGraph=true, ylims=(-2, 1))
 Export("d:\\qpt1", x, e)
