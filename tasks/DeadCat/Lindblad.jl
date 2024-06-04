ef50205b698dfcb30a14c90c5b98c0ebe45ad39e
Pavel Stránský <github@pavelstransky.cz>
2024-06-04 15:41:14 +0200

    Simple Lindblad equation

--- tasks/DeadCat/Lindblad.jl
+++ tasks/DeadCat/Lindblad.jl
@@ -5,9 +5,9 @@ pyplot(size=(1000, 1000))
 
 BLAS.set_num_threads(1)     # To prevent the Stack Overflow error
 
-const PATH = "d:/results/Rabi/deadcat/X"
+const PATH = "d:/results/Rabi/deadcat/"
 
-R = 100.0
+R = 50.0
 μ = 0.13 / R
 ν = μ
 
@@ -15,8 +15,8 @@ c = 0.5
 d = 0.02
 
 mint = 0.0
-maxt = 500.0
-numt = 1000
+maxt = 30.0
+numt = 300
 
 rabi = Rabi(R=R, λ=0.75, δ=0.5, μ=μ, ν=ν)
 result = ExpectationValues(rabi, [:Jx=>Jx(rabi), :Jy=>Jy(rabi), :Jz=>Jz(rabi), :q=>X(rabi), :p=>P(rabi), :n=>N(rabi)]; mint=mint, maxt=maxt, numt=numt, showGraph=true, saveGraph=true, saveData=true, asymptotics=false)
