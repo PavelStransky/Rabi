c74264c61139da33308bc8d4a34b0f948a190cfd
Pavel Stránský <github@pavelstransky.cz>
2024-06-07 09:52:31 +0200

    Parameters for IPNP36

--- tasks/DeadCat/X.jl
+++ tasks/DeadCat/X.jl
@@ -4,11 +4,12 @@ using Distributed
 
 include("../../Calculation.jl")
 pyplot(size=(1200, 1200))
-const PATH = "d:/results/Rabi/deadcat/f3/"
+# const PATH = "d:/results/Rabi/deadcat/f3/"
+const PATH = ""
 
-R = 1000.0
-# μ = 0.135341379 / R
-μ = 1.43 / R
+R = 10000.0
+μ = 0.132 / R
+# μ = 1.43 / R
 
 rabi = Rabi(R=R, λ=0.75, δ=0.5, μ=μ, ν=μ)
-result = ExpectationValues(rabi, [:x => X(rabi)]; mint=0.0, maxt=50.0, numt=1001, showGraph=true, saveGraph=false, saveData=true, asymptotics=false)
+result = ExpectationValues(rabi, [:x => X(rabi)]; mint=0.0, maxt=100.0, numt=1001, showGraph=true, saveGraph=false, saveData=true, asymptotics=false)
