747bf70ea4ebdb70351369f0f567c7cf0c9082a8
Pavel Stránský <github@pavelstransky.cz>
2025-04-15 10:33:06 +0200

    Modify figures combining

--- tasks/CombineFigures.py
+++ tasks/CombineFigures.py
@@ -1,7 +1,8 @@
 import os
 from PIL import Image
+from alive_progress import alive_bar
 
-dir = 'D:/results/Rabi/schnellbruder'
+dir = 'D:/results/Rabi/schnellbruder/3 brothers'
 
 fnamesSphere = []
 indexSphere = []
@@ -19,13 +20,12 @@ for fname in os.listdir(dir):
     indexSphere.append(float(fname[start:end]))
 
 fnamesSphere = [x for _, x in sorted(zip(indexSphere, fnamesSphere))]
-print(fnamesSphere)
 
 fnamesWigner = []
 indexWigner = []
 
 for fname in os.listdir(dir):
-    start = fname.find("0.5)_")
+    start = fname.find("hers_")
     end = fname.find(".png")
 
     if start == -1 or end == -1:
@@ -37,26 +37,26 @@ for fname in os.listdir(dir):
     indexWigner.append(float(fname[start:end]))
 
 fnamesWigner = [x for _, x in sorted(zip(indexWigner, fnamesWigner))]
-print(fnamesWigner)
 
+with alive_bar(len(fnamesWigner), title='Combining images') as bar:
+    for i, fname in enumerate(fnamesWigner):
+        srcWigner = f'{dir}/{fname}'
+        srcSphere = f'{dir}/{fnamesSphere[i]}'
+        dst = f'{dir}/{i}.png'
 
-for i, fname in enumerate(fnamesWigner):
-    srcWigner = f'{dir}/{fname}'
-    srcSphere = f'{dir}/{fnamesSphere[i]}'
-    dst = f'{dir}/{i}.png'
+        # Load the images
+        imgWigner = Image.open(srcWigner)
+        imgSphere = Image.open(srcSphere)
 
-    # Load the images
-    imgWigner = Image.open(srcWigner)
-    imgSphere = Image.open(srcSphere)
+        imgSphere = imgSphere.resize((1000, 1000))
+        imgSphere = imgSphere.crop((250, 250, 750, 750))
+        # imgWigner = imgWigner.resize((1920, 1080))
 
-    imgSphere = imgSphere.resize((1000, 1000))
-    imgSphere = imgSphere.crop((250, 250, 750, 750))
-    imgWigner = imgWigner.resize((1920, 1080))
+        # Create a new image with the combined width
+        combined = Image.new("RGBA", (1920, 1080))
+        combined.paste(imgWigner, (0, 0))
+        combined.paste(imgSphere, (1920 - 550, 1080 - 520))
 
-    # Create a new image with the combined width
-    combined = Image.new("RGBA", (1920, 1080))
-    combined.paste(imgWigner, (0, 0))
-    combined.paste(imgSphere, (1920 - 550, 1080 - 520))
-
-    # Save the result
-    combined.save(dst)
+        # Save the result
+        combined.save(dst)
+        bar()
--- tasks/RenameFile.py
+++ tasks/RenameFile.py
@@ -3,11 +3,11 @@ import os
 fnames = []
 energies = []
 
-dir = 'D:/results/Rabi/schnellbruder/2 sphere'
+dir = 'D:/results/Rabi/schnellbruder/3 brothers'
 
 for fname in os.listdir(dir):
-    # start = fname.find("0.5)_") + 5
-    start = fname.find("here_") + 5
+    start = fname.find("0.5)_") + 5
+    # start = fname.find("here_") + 5
     end = fname.find(".png")
 
     if start == -1 or end == -1:
@@ -21,7 +21,7 @@ print(fnames)
 
 for i, fname in enumerate(fnames):
     src = f'{dir}/{fname}'
-    dst = f'{dir}/{i}.png'
+    dst = f'{dir}/brothers_{i+1}.png'
 
     os.rename(src, dst)
 
