19afd450d683c6e222670a64e26e2e7694c80c2e
Pavel Stránský <github@pavelstransky.cz>
2025-04-16 06:57:54 +0200

    New jobs

--- tasks/CreateJobs.py
+++ tasks/CreateJobs.py
@@ -1,7 +1,10 @@
 with open("jobs.txt", "w") as f:
     for i in range(200):
-        f.write(f"nice -20 julia111 Schnellbruder.jl {i} 0")
+        f.write(f"nice -20 julia111 Schnellbruder.jl {i} 10")
         f.write("\n")
     for i in range(200):
-        f.write(f"nice -20 julia111 Schnellbruder.jl {i} 2")
+        f.write(f"nice -20 julia111 Schnellbruder.jl {i} 11")
+        f.write("\n")
+    for i in range(200):
+        f.write(f"nice -20 julia111 Schnellbruder.jl {i} 12")
         f.write("\n")
